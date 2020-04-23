5c872e25f6a2c155da81d73c8fff1b002123eb6f
AidilZartesya <aidilggwp@gmail.com>
2020-04-23 22:54:50 +0700

    new laravel-api

--- larashop-api/database/migrations/2014_10_12_000000_create_users_table.php
+++ larashop-api/database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
@@ -14,23 +14,13 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-
             $table->rememberToken();
-            $table->string('api_token', 80)->unique()->nullable()->default(null);
-			$table->text("address")->nullable();
-			$table->string("phone")->nullable();
-            $table->string("avatar")->default('avatars/no_avatar.png');
-
-            $table->integer('role_id')->unsigned();
-			$table->foreign('role_id')->references('id')->on('roles');
-            
             $table->timestamps();
-			$table->softDeletes();
         });
     }
 
@@ -41,10 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-		Schema::table('users', function(Blueprint $table){
-            $table->dropForeign('users_role_id_foreign');
-        });	
-		
         Schema::dropIfExists('users');
     }
 }
